3264806a628dbf4c2a8c6f78c2a6b2090eac3b6c
mindnbytes <mindnbytes@gmail.com>
2026-06-27 16:51:19 +0200

    Update for the current state

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,7 @@
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -U EDITOR nvim
+    set -U EDITOR hx
     starship init fish | source
     zoxide init fish | source
 end
-
-# uv
-fish_add_path "/Users/alex/.local/bin"
