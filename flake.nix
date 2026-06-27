a1b9e0f9f28f52624939e1c7fad074e3eae9097f
mindnbytes <mindnbytes@gmail.com>
2026-06-27 17:42:14 +0200

    Update flake to produce macbook home configuration as well

--- flake.nix
+++ flake.nix
@@ -1,29 +1,55 @@
 {
-  description = "NixOs from Scratch";
-  
+  description = "NixOs and Home Manager dotfiles";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-26.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.alex = import ./home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          # Uncomment if you need proprietary packages
+          # config.allowUnfree = true;
+        };
+    in
+    {
+      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/nixos-btw/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+
+            home-manager.users.alex = import ./home/profiles/nixos-mini.nix;
+          }
+        ];
+      };
+
+      homeConfigurations.alex-macbook = home-manager.lib.homeManagerConfiguration {
+        pkgs = mkPkgs "aarch64-darwin";
+
+        modules = [
+          ./home/profiles/macbook.nix
+        ];
+      };
     };
-  };
 }
