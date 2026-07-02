044a30f751d6fed48e3deca95d8f41fae03499c4
mindnbytes <mindnbytes@gmail.com>
2026-07-02 21:33:11 +0200

    Add brew; still using some casks

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,6 +10,13 @@ if test -d /nix/var/nix/profiles/default/bin
     fish_add_path --global /nix/var/nix/profiles/default/bin
 end
 
+# Homebrew on macOS
+if test -x /opt/homebrew/bin/brew
+    fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+else if test -x /usr/local/bin/brew
+    fish_add_path /usr/local/bin /usr/local/sbin
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U EDITOR hx
