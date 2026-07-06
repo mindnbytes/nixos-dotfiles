981f27fc228f2f989a55e1e342eb604d987812fe
mindnbytes <mindnbytes@gmail.com>
2026-07-06 20:31:06 +0200

    Added /srv mount to storage config

--- hosts/nixos-btw/storage.nix
+++ hosts/nixos-btw/storage.nix
@@ -1,13 +1,23 @@
 { ... }:
-
+let
+  commonFsType = "ext4";
+  commonOptions = [
+    "nofail"
+    "x-systemd.automount"
+    "x-systemd.device-timeout=60s"
+  ];
+in
 {
-  fileSystems."/mnt/backup" = {
-    device = "/dev/disk/by-label/backup-hdd";
-    fsType = "ext4";
-    options = [
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.device-timeout=60s"
-    ];
+  fileSystems = {
+    "/mnt/backup" = {
+      device = "/dev/disk/by-label/backup-hdd";
+      fsType = commonFsType;
+      options = commonOptions;
+    };
+    "/srv" = {
+      device = "/dev/disk/by-label/services";
+      fsType = commonFsType;
+      options = commonOptions;
+    };
   };
 }
