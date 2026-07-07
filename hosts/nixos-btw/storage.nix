2f69e36818a592cc37127d115e415cd450c51679
mindnbytes <mindnbytes@gmail.com>
2026-07-07 07:48:58 +0200

    Update fs options on ssd

--- hosts/nixos-btw/storage.nix
+++ hosts/nixos-btw/storage.nix
@@ -1,23 +1,24 @@
 { ... }:
 let
   commonFsType = "ext4";
-  commonOptions = [
-    "nofail"
-    "x-systemd.automount"
-    "x-systemd.device-timeout=60s"
-  ];
 in
 {
   fileSystems = {
     "/mnt/backup" = {
       device = "/dev/disk/by-label/backup-hdd";
       fsType = commonFsType;
-      options = commonOptions;
+      options = [
+        "nofail"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=60s"
+      ];
     };
     "/srv" = {
       device = "/dev/disk/by-label/services";
       fsType = commonFsType;
-      options = commonOptions;
+      options = [
+        "x-systemd.device-timeout=60s"
+      ];
     };
   };
 }
