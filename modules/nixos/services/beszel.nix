3e2ad9e2550db8243b8ffa83f165646b5fbd7381
mindnbytes <mindnbytes@gmail.com>
2026-07-17 20:40:57 +0200

    Add beszel agent config

--- modules/nixos/services/beszel.nix
+++ modules/nixos/services/beszel.nix
@@ -1,11 +1,23 @@
 { ... }:
 
 {
-  services.beszel.hub = {
-    enable = true;
+  services.beszel = {
+    hub = {
+      enable = true;
 
-    environment = {
-      APP_URL = "http://monitor.home.arpa";
+      environment = {
+        APP_URL = "http://monitor.home.arpa";
+      };
+    };
+    agent = {
+      enable = true;
+
+      environment = {
+        LISTEN = "127.0.0.1:45876";
+        SYSTEM_NAME = "nixos-btw";
+      };
+
+      environmentFile = "/var/lib/secrets/beszel-agent.env";
     };
   };
 }
